373be0e281701f99510050202780586778849b84
VictoriaDun <victoria.dunas@yahoo.com>
2019-09-12 10:01:27 +0200

    calcul1

--- 05-Javascript/02-maths/01-calculator-one/script.js
+++ 05-Javascript/02-maths/01-calculator-one/script.js
@@ -12,24 +12,42 @@
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("addition").addEventListener("click", (displayvalue) => {
-        let operandeun = document.getElementById('op-one').value
-        let operandedeu = document.getElementById('op-two').value
-        let displayvalue
-        alert displayvalue ()
+    document.getElementById("addition").addEventListener("click", () => {
+        let opun, opdeux, somme; 
+        operandeun = document.getElementById('op-one').value;
+        operandedeux = document.getElementById('op-two').value;
+        somme = parseInt(operandeun) + parseInt(operandedeux);
+        document.getElementById('addition').value= somme;
+        alert('result='+ somme)
+     
 
         // perform an addition
     });
 
     document.getElementById("substraction").addEventListener("click", () => {
-        // perform an substraction
+        let opun, opdeux, substr; 
+        operandeun = document.getElementById('op-one').value;
+        operandedeux = document.getElementById('op-two').value;
+        substr = parseInt(operandeun) - parseInt(operandedeux);
+        document.getElementById('substraction').value= substr;
+        alert('result='+ substr)// perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", () => {
-        // perform an multiplication
+        let opun, opdeux, mult; 
+        operandeun = document.getElementById('op-one').value;
+        operandedeux = document.getElementById('op-two').value;
+        mult = parseInt(operandeun) * parseInt(operandedeux);
+        document.getElementById('multiplication').value= mult;
+        alert('result='+ mult)/// perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", () => {
-        // perform an division
+        let opun, opdeux, div; 
+        operandeun = document.getElementById('op-one').value;
+        operandedeux = document.getElementById('op-two').value;
+        div = parseInt(operandeun) / parseInt(operandedeux);
+        document.getElementById('division').value= div;
+        alert('result='+ div)// perform an division
     });
 })();
